2208e533e4cc23f2505c054b1667c963cf059c86
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-03-05 14:26:46 -0300

    list_1

---
+++ list_1_montagem_layout/android/app/src/main/kotlin/com/example/list_1_montagem_layout/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.list_1_montagem_layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ media_query/android/app/src/main/kotlin/com/example/media_query/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.media_query
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
