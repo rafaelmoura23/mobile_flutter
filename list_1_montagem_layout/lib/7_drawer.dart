840c2398db3a1876ff68c3f92133ed31007b2e4e
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-03-11 16:40:41 -0300

    exercicio7

--- list_1_montagem_layout/lib/7_drawer.dart
+++ list_1_montagem_layout/lib/7_drawer.dart
@@ -1,2 +1,54 @@
-// Implemente um menu lateral (Drawer) contendo diversas opções.
-// Configure o menu para ser acessado através de um botão na barra de aplicativo.
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Exercício 7 - Menu Lateral(Drawer)'),
+        ),
+        drawer: Drawer(
+          child: Container(
+            child: ListView(
+              children: [
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 78, 107, 130),
+                  ),
+                  child: Text(
+                    'Drawer',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text('Home'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text('Tela 2'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text('Tela 3'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text('Tela 4'),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
