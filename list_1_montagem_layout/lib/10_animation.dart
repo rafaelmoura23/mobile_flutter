1c9ea2999a16ac8f737c1bcb470a9b9797e1932c
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-03-12 16:00:48 -0300

    exercicio10

--- list_1_montagem_layout/lib/10_animation.dart
+++ list_1_montagem_layout/lib/10_animation.dart
@@ -1,2 +1,63 @@
-// Integre animações simples em elementos do seu layout.
-// Por exemplo, faça um widget mover-se ou mudar de cor ao ser clicado.
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color colorPurple = Colors.deepPurpleAccent;
+
+  void _changeColor(Color color) {
+    setState(() {
+      colorPurple = color;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double boxAltura = 100;
+    double boxLargura = 100;
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Exercicío 2'),
+        ),
+        body: Center(
+          child: AnimatedContainer(
+            duration: Duration(seconds: 2),
+            curve: Curves.fastOutSlowIn,
+            width: boxLargura,
+            height: boxAltura,
+            decoration: BoxDecoration(
+              color: colorPurple,
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () => _changeColor(Colors.blue),
+              icon: Icon(Icons.palette),
+            ),
+            IconButton(
+              onPressed: () => _changeColor(Colors.green),
+              icon: Icon(Icons.palette),
+            ),
+            IconButton(
+              onPressed: () => _changeColor(Colors.red),
+              icon: Icon(Icons.palette),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- list_1_montagem_layout/lib/9_tab_bar.dart
+++ list_1_montagem_layout/lib/9_tab_bar.dart
@@ -1,3 +1,46 @@
 // Crie uma página que faça uso do widget TabBar para organizar conteúdo em diferentes
 // abas.
-// Adicione conteúdo exclusivo para cada aba.
+
+// Implemente uma barra de navegação personalizada, utilizando o widget AppBar e ícones.
+// Crie diferentes abas que levem a seções distintas da sua aplicação.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: DefaultTabController(
+      // numero de abas
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('EXERCÍCIO BARRA DE NAVEGAÇÃO'),
+          bottom: TabBar(
+            // abas
+            tabs: [
+              Tab(icon: Icon(Icons.flight)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.electric_bike))
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            // conteúdo das abas
+            Text("Primeira Aba"),
+            Text("Segunda Aba"),
+            Text("Terceira Aba"),
+            Text("Quarta Aba")
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+}
