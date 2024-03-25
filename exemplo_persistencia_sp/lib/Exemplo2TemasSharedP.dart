41fc5470d67e0cbc6587624b34d1d65f3d1c380d
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-03-25 16:47:31 -0300

    example_dark_theme

--- exemplo_persistencia_sp/lib/Exemplo2TemasSharedP.dart
+++ exemplo_persistencia_sp/lib/Exemplo2TemasSharedP.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light, // Define o tema claro como padrão
       ),
