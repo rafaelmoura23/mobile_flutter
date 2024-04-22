927b92a78adf14f1e0f536c309dc26563f51ab03
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-04-22 16:09:15 -0300

    persistencia de dados com json

---
+++ exemplo_persistencia_json/lib/Model/ProdutosModel.dart
@@ -0,0 +1,23 @@
+class Produto {
+  final String nome;
+  final String preco;
+  final String categoria;
+
+  Produto({required this.nome, required this.preco, required this.categoria});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nome' : nome,
+      'preco' : preco,
+      'categoria' : categoria,
+    };
+  }
+
+  factory Produto.fromJson(Map<String, dynamic> map) {
+    return Produto(
+      nome: map['nome'],
+      preco: map['preco'],
+      categoria: map['categoria'],
+    );
+  }
+}
