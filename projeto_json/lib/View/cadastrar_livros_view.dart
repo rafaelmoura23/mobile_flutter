fb5f8082aa56896b0ca6bc4674bef1ca0a00dd04
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-04-30 15:20:20 -0300

    class 'cadastrar_livros_screen' : add controllers and validators

--- projeto_json/lib/View/cadastrar_livros_view.dart
+++ projeto_json/lib/View/cadastrar_livros_view.dart
@@ -8,20 +8,75 @@ class CadastrarLivrosScreen extends StatefulWidget {
 }
 
 class _CadastrarLivrosScreenState extends State<CadastrarLivrosScreen> {
+  // Create controllers
+  final TextEditingController _tituloController =  TextEditingController();
+  final TextEditingController _autorController = TextEditingController();
+  final TextEditingController _editoraController = TextEditingController();
+  final TextEditingController _sinopseController = TextEditingController();
+  final TextEditingController _categoriaController = TextEditingController();
+  final TextEditingController _isbnController = TextEditingController();
+  final TextEditingController _precoController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cadastro de Livros'),
+        title: Text('Cadastrar Livro'),
       ),
-      body: Padding(
+      body: Center(
+        child: Padding(
         padding: EdgeInsets.all(16),
-        child: Center(
+        child: Form(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Título do Livro',
+                ),
+                controller: _tituloController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Insira um título';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Autor',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Editora',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Sinopse',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Categoria',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'ISBN',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Preço',
+                ),
+              )
+            ],
           ),
         ),
+      ),
       ),
     );
   }
