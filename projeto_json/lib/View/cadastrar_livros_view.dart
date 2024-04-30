5044cd740af065097d7b97c538bec587d992565d
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-04-30 15:23:11 -0300

    class 'cadastrar_livros_screen' : add controllers and validators

--- projeto_json/lib/View/cadastrar_livros_view.dart
+++ projeto_json/lib/View/cadastrar_livros_view.dart
@@ -9,14 +9,13 @@ class CadastrarLivrosScreen extends StatefulWidget {
 
 class _CadastrarLivrosScreenState extends State<CadastrarLivrosScreen> {
   // Create controllers
-  final TextEditingController _tituloController =  TextEditingController();
+  final TextEditingController _tituloController = TextEditingController();
   final TextEditingController _autorController = TextEditingController();
   final TextEditingController _editoraController = TextEditingController();
   final TextEditingController _sinopseController = TextEditingController();
   final TextEditingController _categoriaController = TextEditingController();
   final TextEditingController _isbnController = TextEditingController();
   final TextEditingController _precoController = TextEditingController();
-
 
   @override
   Widget build(BuildContext context) {
@@ -26,57 +25,63 @@ class _CadastrarLivrosScreenState extends State<CadastrarLivrosScreen> {
       ),
       body: Center(
         child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Título do Livro',
+          padding: EdgeInsets.all(16),
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Título do Livro',
+                  ),
+                  controller: _tituloController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Insira um título';
+                    }
+                    return null;
+                  },
                 ),
-                controller: _tituloController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Insira um título';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Autor',
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Autor',
+                  ),
+                  controller: _autorController,
                 ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Editora',
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Editora',
+                  ),
+                  controller: _editoraController,
                 ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Sinopse',
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Sinopse',
+                  ),
+                  controller: _sinopseController,
                 ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Categoria',
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Categoria',
+                  ),
+                  controller: _categoriaController,
                 ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'ISBN',
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'ISBN',
+                  ),
+                  controller: _isbnController,
                 ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Preço',
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Preço',
+                  ),
+                  controller: _precoController  ,
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
