e480d483398472b7bbecbc05b68f3a4f7110264b
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-05-13 14:27:15 -0300

    api

--- exemplo_api/lib/screen.dart
+++ exemplo_api/lib/screen.dart
@@ -1,3 +1,4 @@
+import 'package:exemplo_api/service.dart';
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatefulWidget {
@@ -8,8 +9,58 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  final WeatherService _weatherService = WeatherService(
+    apiKey:
+        '681126f28e7d6fa3a7cfe0da0671e599', // Chave de API para acesso à API de previsão do tempo.
+    baseUrl:
+        'https://api.openweathermap.org/data/2.5', // URL base da API de previsão do tempo.
+  );
+
+  late Map<String, dynamic> _weatherData;
+
+  @override
+  void initState() {
+    super.initState();
+    // _weatherService.getWeather('Rio de Janeiro');
+    _fetchWeatherData('Rio de Janeiro');
+  }
+
+  Future<void> _fetchWeatherData(String city) async {
+    try {
+      // Obtém os dados de previsão do tempo para a cidade especificada.
+      final weatherData = await _weatherService.getWeather(city);
+      // Atualiza o estado do widget com os novos dados de previsão do tempo.
+      setState(() {
+        _weatherData = weatherData;
+      });
+    } catch (e) {
+      // Em caso de erro ao buscar os dados de previsão do tempo, exibe uma mensagem de erro no console.
+      print('Error fetching weather data: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Weather Forecat'),
+        ),
+        body: _weatherData == null
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                        'City: ${_weatherData['name']}'), // Exibe o nome da cidade.
+                    Text(
+                        'Temperature: ${_weatherData['main']['temp'] - 273} °C'), // Exibe a temperatura em graus Celsius.
+                    Text(
+                        'Description: ${_weatherData['weather'][0]['description']}'),
+                  ],
+                ),
+              ));
   }
 }
