aed48f7518d7d954c3f1d9694356ed2341b59ab8
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-05-20 15:50:30 -0300

    json git

--- projeto_json_atual/lib/Controller/produtos_controller.dart
+++ projeto_json_atual/lib/Controller/produtos_controller.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
-
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:projeto_json_atual/Model/produtos_model.dart';
 
 class ProdutosController {
@@ -8,14 +7,28 @@ class ProdutosController {
   List<Produto> produtos = [];
 
   Future<List<Produto>> loadProdutos() async {
-    // Carrega o conteúdo do arquivo JSON da pasta assets em uma String
-    final data = await rootBundle.loadString('assets/produtos.json');
-    print(data);
-    // DECODIFICA o conteúdo JSON em uma lista dinâmica
-    final jsonList = json.decode(data) as List<dynamic>;
-    produtos.clear();
-    // Converte os objetos JSON em instâncias de Produto e adiciona à lista de produtos
-    produtos.addAll(jsonList.map((e) => Produto.fromJson(e)));
-    return produtos;
+    try {
+      // URL do arquivo JSON na nuvem
+      final String url = 'https://raw.githubusercontent.com/rafaelmoura23/mobile_flutter/main/produtos.json';
+
+      // Faz a solicitação HTTP para obter o conteúdo do arquivo JSON
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        // Decodifica o conteúdo JSON em uma lista dinâmica
+        final jsonList = json.decode(response.body) as List<dynamic>;
+        produtos.clear();
+        // Converte os objetos JSON em instâncias de Produto e adiciona à lista de produtos
+        produtos.addAll(jsonList.map((e) => Produto.fromJson(e)));
+        return produtos;
+      } else {
+        // Se a solicitação falhar, lança uma exceção
+        throw Exception('Falha ao carregar produtos');
+      }
+    } catch (e) {
+      // Captura e trata qualquer exceção ocorrida durante o processo
+      print('Erro ao carregar produtos: $e');
+      rethrow; // Re-lança a exceção para que ela seja tratada em um nível superior, se necessário
+    }
   }
 }
