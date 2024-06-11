650f9ce93f369d576a041defee49656c2252b4c0
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-06-11 16:51:19 -0300

    music player

--- exemplo_mplayer/lib/screens/music_player_screen.dart
+++ exemplo_mplayer/lib/screens/music_player_screen.dart
@@ -1,5 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+
 import '../models/music_model.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -11,12 +12,34 @@ class MusicPlayerScreen extends StatefulWidget {
 }
 
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
-  late AudioPlayer _audio;
+  late AudioPlayer _audio = AudioPlayer();
+  bool _isPlaying = false;
 
   @override
   void initState() {
     super.initState();
     _audio = AudioPlayer();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    super.setState(fn);
+    _isPlaying = !_isPlaying;
+  }
+
+  @override
+  void dispose() {
+    _audio.dispose();
+    super.dispose();
+  }
+
+  void _playPause() {
+    if (_isPlaying) {
+      _audio.pause();
+    } else {
+      _audio.play(UrlSource(widget.music.url));
+    }
   }
 
   @override
@@ -26,11 +49,10 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
         title: Text(widget.music.title),
       ),
       body: Center(
-        child: TextButton(
-          onPressed: () async {
-            await _audio.play(widget.music.url as Source);
-          },
-          child: const Text("Play"),
+        child: IconButton(
+          icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+          iconSize: 64.0,
+          onPressed: _playPause,
         ),
       ),
     );
